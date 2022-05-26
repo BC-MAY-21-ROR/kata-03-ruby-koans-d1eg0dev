a70ed6bc9bf8b0c4628e922982baa36f311f3c95
d1eg0dev <diego@studiowd.com.mx>
2022-05-25 19:23:21 -0500

    Koans 281/281 solved. Editing README.md

--- setup/README.md
+++ setup/README.md
@@ -1,3 +1,12 @@
 # Setup
 
 Escribe aquí los pasos para ejecutar/probar tu proyecto y el nombre de los integrantes de tu equipo
+
+Pasos para setup de proyecto Ruby Koans
+1.- Descargar Koans de la página oficial http://rubykoans.com/
+2.- Instalar repositorio en tu local
+3.- Descomprimir el archivo koans en tu repositorio
+4.- Probar que koans funcione en la terminal con el código 'rake'
+5.- Una vez seteado el proyecto, Comenzar a resolver los Koans
+
+Diego Eduardo Larios Mejia
---
+++ triangle.rb
@@ -0,0 +1,38 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+  # if any side is equal to zero - it's not a triangle
+	if ( a <= 0 || b <= 0 || c <= 0 ) then
+		raise TriangleError, "Sides of triangle can't be less or equal to zero"
+	# if the sum of two sides less or equal to one side: it's not a triangle
+	elsif ( a >= b+c || b >= a+c || c >= a+b ) then
+		raise TriangleError, "This is not a triangle"
+	else
+	  if a == b && b == c then
+	  	#if 2 sides are equal
+	  	:equilateral 
+	  elsif a == b || a == c || b == c then
+	  	#if all 3 sides are equal
+	  	:isosceles
+	  else
+	  	:scalene
+	  end
+	end
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
